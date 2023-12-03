d4d190019900ac1201996ee79b6fafeaf83ae703
Niky1992 <138252944+Niky1992@users.noreply.github.com>
2023-12-03 16:04:22 +0300

    написал програму

---
+++ Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿﻿string[] array = { "С", "Новым!", "2023", "Годом", "!!!" }; 
+
+PrintArray(array);  
+
+Console.Write(" -> ");
+
+int lengthNewArray = ArrLength(array);                     
+                                                           
+string[] resArray = NewArray(array);                       
+
+PrintArray(resArray);                                    
+
+Console.WriteLine();
+
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+
+int ArrLength(string[] array)                              
+{                                                          
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+string[] NewArray(string[] array)                      
+{
+    int index = 0;
+    
+    string[] resArray = new string[lengthNewArray];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            resArray[index] = array[i];
+            index++;
+        }
+    }
+    return resArray;
+}
+
